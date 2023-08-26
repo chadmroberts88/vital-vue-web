4c6178caf66b1c30b4cdb946a08c107e92810888
chadmroberts88 <chadmroberts88@gmail.com>
2023-08-26 14:29:21 +0000

    Fix for AntDesign table height.

--- src/components/PatientsTableNew.tsx
+++ src/components/PatientsTableNew.tsx
@@ -1,115 +1,210 @@
-import React from "react";
-import { Pagination, Row, Space, Table, Tag } from "antd";
-import type { TableProps } from "antd";
-
+import { Table } from "antd";
+import type { TablePaginationConfig, TableProps } from "antd";
 interface DataType {
+  id: string;
   key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
+  firstName: string;
+  lastName: string;
+  healthcareNumber: string;
+  bloodPressure: string;
+  specificOxygen: number;
+  pulse: number;
+  respirations: number;
 }
 
 const columns: TableProps<DataType>["columns"] = [
   {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    render: (text) => <a>{text}</a>,
+    title: "First name",
+    key: "firstName",
+    dataIndex: "firstName",
   },
   {
-    title: "Age",
-    dataIndex: "age",
-    key: "age",
+    title: "Last name",
+    key: "lastName",
+    dataIndex: "lastName",
   },
   {
-    title: "Address",
-    dataIndex: "address",
-    key: "address",
+    title: "Healthcare #",
+    key: "healthcareNumber",
+    dataIndex: "healthcareNumber",
   },
   {
-    title: "Tags",
-    key: "tags",
-    dataIndex: "tags",
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? "geekblue" : "green";
-          if (tag === "loser") {
-            color = "volcano";
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
+    title: "Blood Pressure",
+    key: "bloodPressure",
+    dataIndex: "bloodPressure",
+    align: "right",
   },
   {
-    title: "Action",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
+    title: "SpO2 (%)",
+    key: "specificOxygen",
+    dataIndex: "specificOxygen",
+    align: "right",
+  },
+  {
+    title: "Pulse (/min)",
+    key: "pulse",
+    dataIndex: "pulse",
+    align: "right",
+  },
+  {
+    title: "Resp. (/min)",
+    key: "respirations",
+    dataIndex: "respirations",
+    align: "right",
   },
 ];
 
 const data: DataType[] = [
   {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    tags: ["nice", "developer"],
+    id: "1234568",
+    key: "01",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "1",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
   },
   {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    tags: ["loser"],
+    id: "87654321",
+    key: "02",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "2",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
   },
   {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sydney No. 1 Lake Park",
-    tags: ["cool", "teacher"],
+    id: "87654320",
+    key: "03",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "3",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654319",
+    key: "04",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "14",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654318",
+    key: "05",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "125",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654317",
+    key: "06",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "17",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654316",
+    key: "07",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "09",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654315",
+    key: "08",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "1230",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654314",
+    key: "09",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "17890",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654313",
+    key: "10",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "1234x7890",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654",
+    key: "11",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "1234x7890",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "854313",
+    key: "12",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "1234x7890",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
   },
 ];
 
-const pagination = {
-  current: 1,
-  total: 0,
-  pageSize: 10,
-};
-
-const TableFooter = () => {
-  return (
-    <Row justify="space-between">
-      <Row>Some Left Side Content...</Row>
-      <Pagination {...pagination} />
-    </Row>
-  );
-};
-
 const PatientsTable = () => {
+  const paginationConfig: TablePaginationConfig = {
+    pageSize: 5,
+  };
+
   return (
     <Table
       style={{
         width: "100%",
+        height: "394px",
         margin: "20px",
         borderRadius: "10px",
-        overflow: "hidden",
         boxShadow: "0px 10px 10px 5px #a9a9a9a9",
+        backgroundColor: "#FAFAFA",
       }}
-      footer={() => <TableFooter />}
-      pagination={false}
+      pagination={paginationConfig}
       columns={columns}
       dataSource={data}
     />
