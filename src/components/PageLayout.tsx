3865df6323de006b1d1e33ce82a174be2806877e
chadmroberts88 <chadmroberts88@gmail.com>
2023-08-31 19:23:55 -0230

    Add sider button. Make sider fixed.

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,10 +1,10 @@
 import { PropsWithChildren, useState } from "react";
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
+  CaretLeftFilled,
+  CaretRightFilled,
 } from "@ant-design/icons";
 import { Layout, Menu, Button } from "antd";
 import Sider from "antd/es/layout/Sider";
@@ -15,7 +15,16 @@ const PageLayout = ({ children }: PageLayoutProps) => {
   const [collapsed, setCollapsed] = useState(true);
 
   return (
-    <Layout style={{ minHeight: "100vh" }}>
+    <Layout
+      style={{
+        minHeight: "100vh",
+        position: "absolute",
+        width: "100%",
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
+    >
       <Sider
         trigger={null}
         collapsible
@@ -23,16 +32,55 @@ const PageLayout = ({ children }: PageLayoutProps) => {
         onCollapse={(value) => setCollapsed(value)}
       >
         <div
-          style={{ display: "flex", justifyContent: "flex-end", width: "100%" }}
+          style={{
+            width: "100%",
+            height: "80px",
+            position: "relative",
+          }}
         >
+          <div
+            style={{
+              width: "40px",
+              height: "40px",
+              backgroundColor: "white",
+              borderRadius: "50%",
+              position: "absolute",
+              margin: "20px",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              lineHeight: "24px",
+              top: "50%",
+              left: "80px",
+              marginTop: "-12px",
+              color: "white",
+              fontSize: "24px",
+              textWrap: "nowrap",
+              display: collapsed ? "none" : "block",
+            }}
+          >
+            Vital Vue
+          </div>
           <Button
             type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            icon={collapsed ? <CaretRightFilled /> : <CaretLeftFilled />}
             onClick={() => setCollapsed(!collapsed)}
             style={{
-              fontSize: "16px",
-              width: 80,
-              height: 64,
+              fontSize: "18px",
+              backgroundColor: "white",
+              position: "absolute",
+              width: "26px",
+              height: "26px",
+              top: "50%",
+              right: "-13px",
+              marginTop: "-13px",
+              borderRadius: "50%",
+              border: "2px solid #001529",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
             }}
           />
         </div>
@@ -59,7 +107,7 @@ const PageLayout = ({ children }: PageLayoutProps) => {
           ]}
         />
       </Sider>
-      <Layout>{children}</Layout>
+      <Layout style={{ overflow: "auto" }}>{children}</Layout>
     </Layout>
   );
 };
