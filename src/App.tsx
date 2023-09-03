9e008ac56d90ea98bf8ead7824b4bb02f303bb65
chadmroberts88 <chadmroberts88@gmail.com>
2023-09-03 10:14:59 -0230

    Remove MUI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-
 import PageLayout from "./components/PageLayout";
 import DashboardPage from "./components/DashboardPage";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#1976d2",
-    },
-  },
-});
-
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <PageLayout>
-        <DashboardPage />
-      </PageLayout>
-    </ThemeProvider>
+    <PageLayout>
+      <DashboardPage />
+    </PageLayout>
   );
 }
 
