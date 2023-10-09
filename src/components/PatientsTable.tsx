a373667a55a34c69850cb5d701984669b982b9f8
chadmroberts88 <chadmroberts88@gmail.com>
2023-10-09 20:19:19 -0230

    Updated patient table.

---
+++ src/components/PatientsTable.tsx
@@ -0,0 +1,98 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  {
+    field: "firstName",
+    headerName: "First name",
+    flex: 1,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    flex: 1,
+  },
+  {
+    field: "healthcareNumber",
+    headerName: "Healthcare #",
+    flex: 1,
+  },
+  {
+    field: "bloodPressure",
+    headerName: "Bloodpressure",
+    type: "number",
+    align: "right",
+    width: 150,
+  },
+  {
+    field: "specificOxygen",
+    headerName: "SpO2 (%)",
+    type: "number",
+    align: "right",
+    width: 150,
+  },
+  {
+    field: "pulse",
+    headerName: "Pulse (/min)",
+    type: "number",
+    align: "right",
+    width: 150,
+  },
+  {
+    field: "respirations",
+    headerName: "Resp. (/min)",
+    type: "number",
+    align: "right",
+    width: 150,
+  },
+];
+
+const rows = [
+  {
+    id: "1234568",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "1234567890",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+  {
+    id: "87654321",
+    firstName: "Chad",
+    lastName: "Roberts",
+    healthcareNumber: "1234567890",
+    bloodPressure: `${120}/${80}`,
+    specificOxygen: 97,
+    pulse: 80,
+    respirations: 42,
+  },
+];
+
+export default function DataGridDemo() {
+  return (
+    <Box sx={{ height: "100%", width: "100%" }}>
+      <DataGrid
+        sx={{ width: "100%" }}
+        rows={rows}
+        columns={columns}
+        density="compact"
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+        getRowClassName={(params) =>
+          params.indexRelativeToCurrentPage % 2 === 0 ? "Mui-even" : "Mui-odd"
+        }
+      />
+    </Box>
+  );
+}
